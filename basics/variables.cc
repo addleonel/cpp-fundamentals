b9938e87f37675a33d77761e45c3be18f5155da2
addleonel <cambroni3633@gmail.com>
2021-08-06 19:46:48 -0500

    variables: initializing == Declaring + Assigning

---
+++ basics/variables.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   // Initializing == Declaring + assigning
+   int a = 5;
+
+   // Declaring variables
+   int b, c;
+   int result;
+
+   // Assigning values
+   b = 2;
+   c = 4;
+   
+   // Process
+   result = a + b + c;
+   
+   // print out the result
+   cout<<result;  // 11
+
+   return 0;
+}
