730f51b0452f207ea3416275576e387312aa1d8a
addleonel <cambroni3633@gmail.com>
2021-08-18 14:08:37 -0500

    stringstream begin

---
+++ basics/strings/stringstream_start.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+int main(){
+	std::string message;
+	int num;
+	char ch_comma;
+	std::vector<int> vector_numbers;
+	getline(std::cin, message);
+	std::stringstream ss(message);
+	
+	while(ss >> num >> ch_comma)
+		vector_numbers.push_back(num);
+
+	ss >> num >> ch_comma;
+	vector_numbers.push_back(num);
+
+	for(auto i = vector_numbers.begin(); i != vector_numbers.end();++i)
+		std::cout<< *i << std::endl;
+
+	return 0;
+}
