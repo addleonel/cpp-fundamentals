a0fc2dcb71b29537f8208e42592a76d501601100
addleonel <adleonel3633@gmail.com>
2021-12-22 12:43:57 -0500

    srand time 0

--- basics/functions/random_numbers.cc
+++ basics/functions/random_numbers.cc
@@ -8,13 +8,15 @@ void print_random_number_srand(int from_, int to_, int count);
 int main() {
     print_random_number(1, 100, 6);
     print_random_number_srand(1, 100, 6);
+    print_random_number_srand(5, 10, 6);
+    print_random_number_srand(10, 20, 6);
     return 0;
 }
 
 void print_random_number(int from_, int to_, int count) {
     std::cout << count << " random numbers between " << from_ << " and " << to_ << "\n";
     for (int i=0; i<count; i++) {
-        std::cout << 1 + rand() % 100 << "\n"; 
+        std::cout << from_ + rand() % to_ << "\n"; 
     }
 }
 
@@ -23,6 +25,6 @@ void print_random_number_srand(int from_, int to_, int count) {
     srand(time(0));
     std::cout << count << " random numbers between " << from_ << " and " << to_ << "\n";
     for (int i=0; i<count; i++) {
-        std::cout << 1 + rand() % 100 << "\n"; 
+        std::cout << from_ + rand() % to_ << "\n"; 
     }
 }
