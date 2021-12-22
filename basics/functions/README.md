dbe375fbffd4526a281ddc94ad65bde6bd61bf55
addleonel <adleonel3633@gmail.com>
2021-12-22 11:21:12 -0500

    random numbers

---
+++ basics/functions/README.md
@@ -0,0 +1,5 @@
+## Function in c++
+### Refarences:
+- https://www.cplusplus.com/doc/tutorial/functions/
+- https://en.wikipedia.org/wiki/Forward_declaration
+- https://www.cplusplus.com/reference/cstdlib/srand/
---
+++ basics/functions/random_numbers.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+void print_random_number(int from_, int to_, int count);
+void print_random_number_srand(int from_, int to_, int count);
+
+int main() {
+    print_random_number(1, 100, 6);
+    print_random_number_srand(1, 100, 6);
+    return 0;
+}
+
+void print_random_number(int from_, int to_, int count) {
+    std::cout << count << " random numbers between " << from_ << " and " << to_ << "\n";
+    for (int i=0; i<count; i++) {
+        std::cout << 1 + rand() % 100 << "\n"; 
+    }
+}
+
+void print_random_number_srand(int from_, int to_, int count) {
+    // this function regenerates the truly random numbers every time it is called
+    srand(time(0));
+    std::cout << count << " random numbers between " << from_ << " and " << to_ << "\n";
+    for (int i=0; i<count; i++) {
+        std::cout << 1 + rand() % 100 << "\n"; 
+    }
+}
