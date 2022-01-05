99cd5926b522b50b42f48b222f70fceea57f59da
addleonel <adleonel3633@gmail.com>
2022-01-05 10:55:07 -0500

    recursion

---
+++ basics/functions/recursion.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int factorial(int n) {
+	if (n == 1 || n == 0) {
+		return 1;
+	}else {
+		return n * factorial(n-1);
+	}
+}
+
+
+int main() {
+	std::cout << factorial(0) << "\n";
+	std::cout << factorial(5) << "\n";	
+	return 0;
+}
