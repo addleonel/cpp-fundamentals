756b4c27c3f3c83dda1bd39285e3aa014dad703e
addleonel <adleonel3633@gmail.com>
2022-01-07 12:50:45 -0500

    data types table

--- c/pointers.c
+++ c/pointers.c
@@ -3,9 +3,9 @@
 int main() {
 	int a = 0x7f;
 	int *pa = &a;
-	printf("Value of a: %d \n", a);
-	printf("Memory address of a: %p \n", &a);
-	printf("Value of pointer pa (Memory address of a): %p \n", pa);
-	printf("Memory address of pointer pa: %p \n", &pa);
+	printf("Value of \"a\": %d \n", a);
+	printf("Memory address of \"a\": %p \n", &a);
+	printf("Value of pointer \"pa\" (Memory address of \"a\"): %p \n", pa);
+	printf("Memory address of pointer \"pa\": %p \n", &pa);
 	return 0;
 }
